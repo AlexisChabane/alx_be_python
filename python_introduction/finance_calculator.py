0f9ea3952b47f849a33c50c8bd8add01ede6ce07
Karabo Chabane <alexischabane61@gmail.com>
2024-09-17 21:52:26 +0200

    Updated file

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,9 @@
 #Ask user to enter need variables
 monthly_income = int(input("Enter your monthly income:"))
-expenses = int(input("Enter your total monthly expenses:"))
+monthly_expenses = int(input("Enter your total monthly expenses:"))
 
 #Calculation for monthly savings
-monthly_savings = monthly_income - expenses
+monthly_savings = monthly_income - monthly_expenses
 
 #Calculation for annual compound savings interests
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
