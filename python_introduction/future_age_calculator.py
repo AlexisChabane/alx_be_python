9240e54dbcb1b625f503972e4f081253ea6c1a13
Karabo Chabane <alexischabane61@gmail.com>
2024-09-17 21:27:17 +0200

    Added new file

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+#Ask user to enter their age
+age = int(input("How old are you?"))
+
+#Calculation for their future age if the year is 2023
+future_age = age + 27
+
+print("In 2025, you'll be", future_age, "years old")
