78eb3c2234b78a9fb13573b509e243b15c5bea11
Karabo Chabane <alexischabane61@gmail.com>
2024-09-17 17:46:15 +0200

    Added python file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+addition_of_nums = (number1 + number2)
+sum_of_numbers = (number1 - number2)
+product_of_numbers = (number1 * number2)
+print("The addition of 10 and 5 is" , addition_of_nums)
+print("The subtraction of 10 and 5 is" , sum_of_numbers)
+print("The mulitplication of 10 and 5 is" , product_of_numbers)
+
