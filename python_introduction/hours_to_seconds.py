bd6f0e4ee34501e756360fe4c75811ec7ce78ffc
Karabo Chabane <alexischabane61@gmail.com>
2024-09-17 21:09:31 +0200

    Added a new file

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,8 @@
+#Defined the two variables
+hours = 2
+nums_of_seconds = 60*60
+
+#Formula to calculate the number of seconds in hours
+seconds = hours * nums_of_seconds
+
+print(hours, "hour(s) is", seconds, "seconds")
