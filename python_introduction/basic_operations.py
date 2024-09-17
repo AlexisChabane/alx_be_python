8f2964f66b4f941b5d5873e6131c409d095cc517
Karabo Chabane <alexischabane61@gmail.com>
2024-09-17 17:51:43 +0200

    Updated python file with comments

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,12 @@
-number1 = 10
+#Adding varaibles to perform calculations with
+number1 = 10 
 number2 = 5
+
+#Doing addition, subtraction and multiplication with the variables
 addition_of_nums = (number1 + number2)
 sum_of_numbers = (number1 - number2)
 product_of_numbers = (number1 * number2)
+
 print("The addition of 10 and 5 is" , addition_of_nums)
 print("The subtraction of 10 and 5 is" , sum_of_numbers)
 print("The mulitplication of 10 and 5 is" , product_of_numbers)
