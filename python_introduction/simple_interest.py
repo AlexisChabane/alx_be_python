1bac40f27fc9382bee2e060b09e7c5ebb3b3fdee
Karabo Chabane <alexischabane61@gmail.com>
2024-09-17 20:33:13 +0200

    Updated the file

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+#Defined three variables
+principle = 1000
+rate = 0.05
+time = 3
+
+#Calculation formula for the simple intrest over three years
+interest = principle * rate * time
+
+print("The simple interest is:" , interest)
