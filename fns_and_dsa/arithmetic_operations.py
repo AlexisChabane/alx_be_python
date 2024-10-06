a4587dfe688a1277427375c5e24d91f1f2cdca8a
Karabo Chabane <alexischabane61@gmail.com>
2024-10-06 15:52:14 +0200

    Updated file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,13 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case operation if operation == "add":
-            add = lambda num1, num2: num1 + num2
-        case operation if operation == "subtract":
-            subtract = lambda num1, num2: num1 - num2
-        case operation if operation == "multiply":
-            mutiply = lambda num1, num2: num1 * num2
-        case operation if operation == "divide":
-            divide = lambda num1, num2: num1 / num2
-            if num1 or num2 == 0:
-                print("The value you've entred is 0, please enter a number")
-    return perform_operation
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+      if num1 or num2 == 0:
+        return "Error: Division by zero"
+      else:
+         return num1/ num2
         
