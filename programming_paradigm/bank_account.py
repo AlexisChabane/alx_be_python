a8e0440e7b3967739516ab10b542a70ce0a82902
Karabo Chabane <alexischabane61@gmail.com>
2024-10-24 16:00:57 +0200

    Deleted extra bank_account file and moved to the right directory

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,17 +1,16 @@
 class BankAccount:
-    def __init__(self, account_balance):
-        self.account_balance = account_balance(0)
-        
+    def __init__(self, account_balance=0):
+        self.account_balance = account_balance
+
     def deposit(self, amount):
         if amount > 0:
             self.account_balance += amount
-    
+
     def withdraw(self, amount):
         if 0 < amount <= self.account_balance:
-            self.account_balance -= amount
+            self.account_balance -= amount 
             return True
-        else: 
-            return False
-        
+        return False
+
     def display_balance(self):
-        print( f"Current Balance: {self.account_balance} ")
+        print(f"Current Balance: {self.account_balance} ")
